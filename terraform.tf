4723e71a24019083bc2a396ab172d52c8dbc47d5
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-21 15:05:54 +0800

    test

--- terraform.tf
+++ terraform.tf
@@ -7,13 +7,4 @@ terraform {
       name = "d3-homelab"
     }
   }
-
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.63.0"
-    }
-  }
-
-  required_version = ">= 0.6.0"
 }
---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.63.0"
+    }
+  }
+
+  required_version = ">= 0.6.0"
+}
