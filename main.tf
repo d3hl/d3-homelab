cd3d513244b598c490df1a1416abc31746cc3120
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-25 03:04:18 +0800

    test

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-#locals {
-#    name_suffix = "${var.resource_tags["projects"]}-${var.resource_tags["environment"]}"
-#}
-terraform {
-  required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-    }
-  }
-}
+
 
 module "dok-hl-39" {
   source = "./prod/dok-hl-39"
