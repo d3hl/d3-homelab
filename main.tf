a600c6099ddb36052827b64c92c07026a4025fb1
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-25 01:34:45 +0800

    test

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 #locals {
 #    name_suffix = "${var.resource_tags["projects"]}-${var.resource_tags["environment"]}"
 #}
-
-
 module "dok-hl-39" {
   source = "./prod/dok-hl-39"
 }
--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -7,6 +7,11 @@ terraform {
   }
   required_version = ">= 0.6.0"
 }
+#module "lxc" {
+#  source = "../../modules/lxc"
+#}
 module "lxc" {
-  source = "../../modules/lxc"
+  source  = "app.terraform.io/ncdv-org/lxc/proxmox"
+  version = "1.0.0"
+  # insert required variables here
 }
