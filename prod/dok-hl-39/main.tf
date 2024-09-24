49eee17aa76f0ba1a96697ea06c19c472b8928ce
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-25 01:48:26 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.63.0"
-    }
-  }
-  required_version = ">= 0.6.0"
-}
 #module "lxc" {
 #  source = "../../modules/lxc"
 #}
