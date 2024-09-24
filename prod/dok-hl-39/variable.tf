aefe6e6933d792a68ef7c16956d4509c9bad1c4b
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-24 23:50:00 +0800

    test

---
+++ prod/dok-hl-39/variable.tf
@@ -0,0 +1,11 @@
+variable "lxc-common" {
+  type=object({
+    node_name = string
+    vm_id = number
+    cores = number 
+    disk = number
+    memory = number
+    ipv4 = string
+    ct_bridge = string
+  })
+}
