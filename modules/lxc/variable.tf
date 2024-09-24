808bdc10d1e7ffb0aecae37aa2b0c70bd91adaca
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-24 23:34:25 +0800

    test

--- modules/lxc/variable.tf
+++ modules/lxc/variable.tf
@@ -37,6 +37,6 @@ locals {
   ct_datastore_template_location = "local"
   ct_datastore_storage_location  = "local"
   ct_source_file_path            = "http://download.proxmox.com/images/system/debian-12-standard_12.2-1_amd64.tar.zst"
-  dns                            = "192.168.2.10"
+  dns                            = ["192.168.2.10","192.168.2.1"]
   gateway                        = "192.168.2.1"
 }
