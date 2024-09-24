f11264a6d9bbf42f4925c289db8726bb3d59bf0d
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-25 01:43:02 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -11,7 +11,13 @@ terraform {
 #  source = "../../modules/lxc"
 #}
 module "lxc" {
-  source  = "app.terraform.io/ncdv-org/lxc/proxmox"
+  source  =, "app.terraform.io/ncdv-org/lxc/proxmox"
   version = "1.0.0"
-  # insert required variables here
+  node_name = "${var.lxc-common.node_name}"
+  vm_id     = "${var.lxc-common.vm_id}" 
+  cores     = "${var.lxc-common.cores}"
+  disk      = "${var.lxc-common.disk}"
+  memory    = "${var.lxc-common.memory}"
+  ipv4      = "${var.lxc-common.ipv4}"
+  ct_bridge = "${var.lxc-common.ct_bridge}"
 }
