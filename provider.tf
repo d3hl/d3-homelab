d371e52844d64f9fcba56415f27bbf191dd14c4d
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-24 22:31:22 +0800

    test

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
     provider "proxmox" {
-        endpoint = var.d3-pve-credentials.endpoint 
-        username = var.d3-pve-credentials.pve_user
-        password = var.d3-pve-credentials.pve_password 
+        endpoint = "${var.d3-pve-credentials.endpoint}" 
+        username = "${var.d3-pve-credentials.pve_user}"
+        password = "${var.d3-pve-credentials.pve_password}" 
         insecure = true 
     ssh {
         agent = true
-        username = var.pve_user
+        username = "${var.pve_user}"
       }
       }
