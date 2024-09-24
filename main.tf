7a9bc12ba446f61a8a490b31e11a97c20b92cfa4
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-25 04:56:00 +0800

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-variable "proxmox_lxc-common" {
-  
-}
 module "dok-hl-39" {
   source = "./prod/dok-hl-39"
 }
--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -6,18 +6,8 @@ terraform {
     }
   }
 }
-variable "proxmox_lxc-common" {
-  
-}
 module "pve" {
   source  = "app.terraform.io/ncdv-org/pve/d3"
   version = "1.0.0"
-  lxc-common = "${var.proxmox_lxc-common}"
-  node_name = "${var.lxc-common.node_name}"
-  vm_id     = "${var.lxc-common.vm_id}" 
-  cores     = "${var.lxc-common.cores}"
-  disk      = "${var.lxc-common.disk}"
-  memory    = "${var.lxc-common.memory}"
-  ipv4      = "${var.lxc-common.ipv4}"
-  ct_bridge = "${var.lxc-common.ct_bridge}"
+  lxc-common = "${var.pve_lxc-common}"
 }
