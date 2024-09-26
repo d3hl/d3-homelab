ed5e8f6eca68e13f655fc8b445b9a2040a34aff4
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-26 20:00:32 +0800

    test

--- provider.tf
+++ provider.tf
@@ -25,12 +25,13 @@ terraform {
 
 }
     provider "proxmox" {
-        endpoint = "${var.d3-pve-credentials.endpoint}" 
-        username = "${var.d3-pve-credentials.pve_user}"
-        password = "${var.d3-pve-credentials.pve_password}" 
+        endpoint = var.d3-pve-credentials.endpoint 
+       # endpoint = "${var.d3-pve-credentials.endpoint}" 
+        username = var.d3-pve-credentials.pve_user
+        password = var.d3-pve-credentials.pve_password
         insecure = true 
     ssh {
         agent = true
-        username = "${var.d3-pve-credentials.pve_user}"
+        username = var.d3-pve-credentials.pve_user
       }
       }
--- variables.tf
+++ variables.tf
@@ -12,3 +12,25 @@ variable "d3-pve-credentials" {
       publickey = ""
     }
 }
+variable "lxc-common" {
+  type=object({
+    node_name = string
+    vm_id = number
+    hostname = string
+    cores = number 
+    disk = number
+    memory = number
+    ipv4 = string
+    ct_bridge = string
+  })
+  default = {
+    node_name = ""
+    vm_id = 0
+    hostname = ""
+    cores = 0 
+    disk = 0
+    memory = 0
+    ipv4 = ""
+    ct_bridge = ""
+  }
+  }
