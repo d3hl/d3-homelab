0e408ad675add27e47b23bda67002b8b278580f0
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-26 16:58:07 +0800

    test

--- prod/dok-hl-39/variables.tf
+++ prod/dok-hl-39/variables.tf
@@ -3,7 +3,7 @@ variable "lxc-common" {
     node_name = string
     vm_id = number
     cores = number 
-    disksize = number
+    disk = number
     memory = number
     ipv4 = string
     ct_bridge = string
@@ -12,7 +12,7 @@ variable "lxc-common" {
     node_name = ""
     vm_id = 0
     cores = 0 
-    disksize = 0
+    disk = 0
     memory = 0
     ipv4 = ""
     ct_bridge = ""
