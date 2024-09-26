1e8a970e27d5d3ca8e8ecc23b1f0270c02b916e1
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-26 09:25:58 +0800

    test

--- variable.tf
+++ variable.tf
@@ -11,23 +11,6 @@ variable "d3-pve-credentials" {
     }
 }
 
-variable "lxc-common" {
-  type=object({
-    node_name = string
-    vm_id = number
-    cores = number 
-    disk = number
-    memory = number
-    ipv4 = string
-    ct_bridge = string
-  })
-  default = {
-    node_name = ""
-    vm_id     = 0
-    cores     = 0 
-    disk      = 0
-    memory    = 0
-    ipv4      = ""
-    ct_bridge = "" 
-  }
+variable "pve_lxc-common" {
+
 }
