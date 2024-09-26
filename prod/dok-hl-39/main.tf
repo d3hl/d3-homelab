0920972023bee559c9b75d7ca7d74b90e3fc9653
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-26 13:36:30 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -6,26 +6,6 @@ terraform {
     }
   }
 }
-variable "lxc-common" {
-  type=object({
-    node_name = string
-    vm_id = number
-    cores = number 
-    disk = number
-    memory = number
-    ipv4 = string
-    ct_bridge = string
-  })
-  default = {
-    node_name = ""
-    vm_id = 0
-    cores = 0 
-    disk = 0
-    memory = 0
-    ipv4 = ""
-    ct_bridge = "" 
-  }
-}
 
 module "pve" {
   source  = "app.terraform.io/ncdv-org/pve/d3"
