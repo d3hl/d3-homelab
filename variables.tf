2d574be1cb4f710433db1850630d02811b704ab2
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-27 04:10:19 +0800

    test

--- variables.tf
+++ variables.tf
@@ -25,3 +25,24 @@ variable "lxc-common" {
     ct_bridge = ""
   }
   }
+
+  variable "hostname" {
+  type = string
+  default = ""
+}
+variable "cores" {
+  type = number
+  default = null
+}
+variable "memory" {
+  type = number
+  default = null
+}
+variable "ipv4" {
+  type = string
+  default = ""
+}
+variable "ct_bridge" {
+  type = string
+  default = ""
+}
