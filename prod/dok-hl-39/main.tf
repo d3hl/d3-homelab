6ff1e8a09a71a223051a441ee57caa30936e69a4
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-27 02:08:17 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -21,28 +21,12 @@ terraform {
       source = "bpg/proxmox"
       version = "0.65.0"
     }
-    tfe = {
-      source = "hashicorp/tfe"
-      version = "0.58.1"
-    }
   }
 
 }
-
-data "tfe_workspace" "d3-homelab" {
-  name         = "d3-homelab"
-  organization = "ncdv-org"
-}
-
-data "tfe_variables" "d3-homelab" {
-  workspace_id = data.tfe_workspace.d3-homelab.id
-}
-
-
-
 module "pve" {
   source  = "app.terraform.io/ncdv-org/pve/d3"
   version = "1.0.0"
-  lxc-common = module.vars.pve.lxc-common
+  lxc-common = var.lxc-common
 
 }
