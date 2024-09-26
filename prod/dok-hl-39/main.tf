2475b0481d749ee2b96b9e7be53e9055e3174a88
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-26 09:22:44 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -11,10 +11,6 @@ terraform {
 module "pve" {
   source  = "app.terraform.io/ncdv-org/pve/d3"
   version = "1.0.0"
-  cores = "${var.pve_lxc-common.cores}"
-  ct_bridge = "${var.pve_lxc-common.ct_bridge}"
-  disk = "${var.pve_lxc-common.disk}"
-  ipv4 = "${var.pve_lxc-common.ipv4}"
-  memory = "${var.pve_lxc-common.memory}"
+  lxc-common = var.pve_lxc-common
 
 }
