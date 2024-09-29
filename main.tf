9081da486b4423566847688d84ae6eb8fdfd8eb6
d3adc3ii <77715129+d3adc3II@users.noreply.github.com>
2024-09-29 02:29:01 +0000

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 module "dok-hl-39" {
   source = "./prod/dok-hl-39"
-}
+  lxc-common = var.lxc-common
+  endpoint   = var.endpoint
+  pvepassword = var.pvepassword
+  api_token = var.api_token
+  pveuser = var.pveuser
+  }
