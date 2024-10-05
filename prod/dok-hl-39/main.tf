657904f11411640b3d00e63569898ccb05f5a418
d3adc3ii <d3social@pm.me>
2024-10-06 07:43:35 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -23,6 +23,19 @@ terraform {
     }
   }
 
+    provider "proxmox" {
+      endpoint = var.endpoint 
+      username = var.pveuser
+      password = var.pvepassword
+      insecure = true 
+    ssh {
+        agent = true
+        username = var.pveuser
+
+      }
+      }
+
+
 }
 module "lxc" {
   source  = "app.terraform.io/ncdv-org/lxc/pve"
