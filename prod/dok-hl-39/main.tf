e0310411553d27527f4c26fd91945d7477034047
d3adc3ii <d3social@pm.me>
2024-10-06 07:41:01 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -1,15 +1,29 @@
-    provider "proxmox" {
-      endpoint = var.endpoint 
-      username = var.pveuser
-      password = var.pvepassword
-      insecure = true 
-    ssh {
-        agent = true
-        username = var.pveuser
+terraform {
+  required_version = ">= 1.5.0"
+  cloud {
+    organization ="ncdv-org"
+    workspaces {
+      project = "ncdv-hl"
+      name = "d3-homelab"
+    }
+  }
+  required_providers {
 
-      }
-      }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.65.0"
+    }
+  }
 
+}
 module "lxc" {
   source  = "app.terraform.io/ncdv-org/lxc/pve"
   version = "1.0.0"
