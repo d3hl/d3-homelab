0492698ae6663c910b948dd5f59074093daa6836
d3adc3ii <d3social@pm.me>
2024-10-06 07:45:51 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -23,17 +23,7 @@ terraform {
     }
   }
 
-    provider "proxmox" {
-      endpoint = var.endpoint 
-      username = var.pveuser
-      password = var.pvepassword
-      insecure = true 
-    ssh {
-        agent = true
-        username = var.pveuser
 
-      }
-      }
 
 
 }
