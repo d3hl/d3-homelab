aad38c4fc81e88d37c8929fff56eaf18cfbb97c3
d3adc3ii <d3social@pm.me>
2024-10-06 12:03:49 +0800

    test

--- prod/dok-hl-39-delete/main.tf
+++ prod/dok-hl-39-delete/main.tf
@@ -1,30 +1,4 @@
-terraform {
-  required_version = ">= 1.5.0"
-  cloud {
-    organization ="ncdv-org"
-    workspaces {
-      project = "ncdv-hl"
-      name = "d3-homelab"
-    }
-  }
-  required_providers {
 
-    local = {
-      source  = "hashicorp/local"
-      version = "2.5.2"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.6"
-    }
-    proxmox = {
-      source = "bpg/proxmox"
-      version = "0.65.0"
-      alias = "dok-hl-39"
-    }
-  }
-
-}
 module "lxc" {
   source  = "app.terraform.io/ncdv-org/lxc/pve"
   version = "1.0.0"
---
+++ prod/dok-hl-39-delete/provider.tf
@@ -0,0 +1,10 @@
+provider "proxmox" {
+        endpoint = var.endpoint
+        username = var.pveuser
+        password = var.pvepassword
+        insecure = true 
+    ssh {
+        agent = true
+        username = var.pveuser
+      }
+      } 
