c9e8958d9ba2e560ca2fac4fe022cab74a02eea9
d3adc3ii <d3social@pm.me>
2024-10-06 13:57:03 +0800

    test

--- prod/talos-k8s/main.tf
+++ prod/talos-k8s/main.tf
@@ -1,6 +1,6 @@
 module "k8s" {
   source  = "app.terraform.io/ncdv-org/k8s/pve"
-  version = "1.0.1"
+  version = "1.0.2"
   talos-common = var.talos-common
   talos_ips = var.talos_ips
   pveuser = var.pveuser
