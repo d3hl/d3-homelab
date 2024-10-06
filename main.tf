7556ef36798dc992388507b345d77ea8e0081c57
d3adc3ii <d3social@pm.me>
2024-10-06 08:30:51 +0800

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-module "dok-hl-39" {
-  source = "./prod/dok-hl-39"
-  lxc-common = var.lxc-common
-  endpoint   = var.endpoint
-  pvepassword = var.pvepassword
-  api_token = var.api_token
-  pveuser = var.pveuser
-  }
+#module "dok-hl-39" {
+#  source = "./prod/dok-hl-39"
+#  lxc-common = var.lxc-common
+#  endpoint   = var.endpoint
+#  pvepassword = var.pvepassword
+#  api_token = var.api_token
+#  pveuser = var.pveuser
+# }
 module "talos-k8s" {
   source = "./prod/talos-k8s"
   endpoint   = var.endpoint
