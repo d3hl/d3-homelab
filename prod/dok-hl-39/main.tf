dabe0900aeb525fa0e448cf4168c9c17683cec77
d3adc3ii <d3social@pm.me>
2024-10-06 10:58:25 +0800

    test

--- prod/dok-hl-39/main.tf
+++ prod/dok-hl-39/main.tf
@@ -1,13 +1,28 @@
 terraform {
-  
+  required_version = ">= 1.5.0"
+  cloud {
+    organization ="ncdv-org"
+    workspaces {
+      project = "ncdv-hl"
+      name = "d3-homelab"
+    }
+  }
   required_providers {
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
     proxmox = {
-      source  = "bpg/proxmox"
+      source = "bpg/proxmox"
       version = "0.65.0"
     }
   }
 
-  required_version = ">= 0.6.0"
 }
 module "lxc" {
   source  = "app.terraform.io/ncdv-org/lxc/pve"
