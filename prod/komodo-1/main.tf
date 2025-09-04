ddd742b603796f17593dff4fa6fcadfecd5070b9
Jay Dinh <d3tech@pm.me>
2025-09-05 04:51:37 +0800

    x

--- prod/komodo-1/main.tf
+++ prod/komodo-1/main.tf
@@ -2,12 +2,6 @@ module "vm" {
   source  = "app.terraform.io/d3-org/vm/pve"
   version = "1.0.0"
    virtual_environment_endpoint=var.virtual_environment_endpoint
-#  username = var.virtual_environment_username
    virtual_environment_api_token=var.virtual_environment_api_token
- # ssh_username = var.virtual_environment_ssh_username
- # nodename= var.virtual_environment_node_name
- # insecure=var.virtual_environment_insecure
- # storage=virtual_environment_storage
- # url=var.latest_debian_13_bookworm_qcow2_img_url
   
 }
