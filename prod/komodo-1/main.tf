f0110c386142aee536de12ff0b63f1318e19d969
Jay Dinh <d3tech@pm.me>
2025-09-05 07:05:17 +0800

    xx

--- prod/komodo-1/main.tf
+++ prod/komodo-1/main.tf
@@ -3,7 +3,7 @@ module "vm" {
   version = "1.0.6"
   virtual_environment_api_token = "${var.virtual_environment_api_token}"
   virtual_environment_endpoint = "${var.virtual_environment_endpoint}"
-  virtual_environment_node_name = "true"
+  virtual_environment_node_name = "pve10"
   virtual_environment_ssh_username = "d3"
   virtual_environment_storage = "cephfs"
   virtual_environment_username = "root@pam"
