4f3e5c8b46312c7dbf9a1c86f99d7d84e695106d
Jay Dinh <d3tech@pm.me>
2025-09-05 05:55:07 +0800

    x

--- prod/komodo-1/main.tf
+++ prod/komodo-1/main.tf
@@ -5,3 +5,10 @@ module "vm" {
    virtual_environment_api_token=var.virtual_environment_api_token
   
 }
+module "vm" {
+  source  = "app.terraform.io/d3-org/vm/pve"
+  version = "1.0.3"
+
+  virtual_environment_api_token = "${var.vm_virtual_environment_api_token}"
+  virtual_environment_endpoint = "${var.vm_virtual_environment_endpoint}" 
+}
