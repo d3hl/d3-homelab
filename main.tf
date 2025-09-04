90056aeba21b8692510848c6240d499430279089
Jay Dinh <d3tech@pm.me>
2025-09-05 04:23:02 +0800

    x

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ module "komodo-1" {
   source  = "app.terraform.io/d3-org/vm/pve"
   version = "1.0.0"
 #  endpoint=var.virtual_environment_endpoint
- # username = var.virtual_environment_username
   #api=var.virtual_environment_api_token
   #nodename= var.virtual_environment_node_name
   #insecure=var.virtual_environment_insecure
