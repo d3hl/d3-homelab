6dac66b760086ac93d7a74d44c47856c68d783a7
Jay Dinh <d3tech@pm.me>
2025-09-05 07:04:42 +0800

    xxxx

--- prod/komodo-1/main.tf
+++ prod/komodo-1/main.tf
@@ -1,8 +1,8 @@
 module "vm" {
   source  = "app.terraform.io/d3-org/vm/pve"
   version = "1.0.6"
-  virtual_environment_api_token = "${var.vm_virtual_environment_api_token}"
-  virtual_environment_endpoint = "${var.vm_virtual_environment_endpoint}"
+  virtual_environment_api_token = "${var.virtual_environment_api_token}"
+  virtual_environment_endpoint = "${var.virtual_environment_endpoint}"
   virtual_environment_node_name = "true"
   virtual_environment_ssh_username = "d3"
   virtual_environment_storage = "cephfs"
