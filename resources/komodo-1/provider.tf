11b3d2661e0ca19120199b426ffbd3ee4c5cead1
Jay Dinh <d3tech@pm.me>
2025-09-06 01:57:08 +0800

    x

--- resources/komodo-1/provider.tf
+++ resources/komodo-1/provider.tf
@@ -14,9 +14,9 @@ terraform {
     provider "proxmox" {
         endpoint = var.virtual_environment_endpoint
         api_token = var.virtual_environment_api_token
-        insecure = insecure 
+        insecure = true 
     ssh {
         agent = true
-        username = d3 
+        username = "d3"
       }
       }
