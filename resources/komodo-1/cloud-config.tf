aa57fe7f1183de65f866fd2e9d9e194b59a7756c
Jay Dinh <d3tech@pm.me>
2025-09-06 02:40:52 +0800

    x

--- resources/komodo-1/cloud-config.tf
+++ resources/komodo-1/cloud-config.tf
@@ -19,7 +19,7 @@ resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
           - sudo
         shell: /bin/bash
         ssh_authorized_keys:
-          - ${trimspace(var.Z}
+          - ${trimspace(var.ssh_public_key)}
         sudo: ALL=(ALL) NOPASSWD:ALL
     package_update: true
     packages:
