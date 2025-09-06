4b211ac1ea4d3e387bd88601dee02df50daf3562
Jay Dinh <d3tech@pm.me>
2025-09-06 22:59:00 +0800

    x

--- resources/debian-vm/cloud-config.tf
+++ resources/debian-vm/cloud-config.tf
@@ -1,5 +1,5 @@
 data "local_file" "ssh_public_key" {
-  filename = "./.ssh/id_ed.pub"
+  filename = "./.ssh/id_ed.pub" # local public key
 }
 
 resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
