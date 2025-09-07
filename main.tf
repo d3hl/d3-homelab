01517af93db7690010b7b5bdf797209dd7b5146d
Jay Dinh <d3tech@pm.me>
2025-09-07 23:24:20 +0800

    x

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "debian-vm" {
+module "komodo" {
   #source  = "app.terraform.io/d3-org/vm/pve"
-  source  = "./resources/debian-vm"
+  source  = "./modules/komodo"
   virtual_environment_endpoint = "${var.virtual_environment_endpoint}"
   virtual_environment_api_token = "${var.virtual_environment_api_token}"
   virtual_environment_node_name = "${var.virtual_environment_node_name}"
