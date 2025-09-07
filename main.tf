9e8625fb96baf3b3365c9b34c3b53749f5271928
Jay Dinh <d3tech@pm.me>
2025-09-08 06:32:28 +0800

    c

--- main.tf
+++ main.tf
@@ -1,22 +1,12 @@
 data "local_file" "ssh_public_key" {
   filename = "/home/d3/.ssh/id_ed.pub" 
 }
-module "komodo1" {
+module "komodo2" {
   #source  = "app.terraform.io/d3-org/vm/pve"
-  source  = "./modules/komodo1"
+  source  = "./modules/komodo2"
   virtual_environment_endpoint = var.virtual_environment_endpoint
   virtual_environment_api_token = var.virtual_environment_api_token
   virtual_environment_node1_name = var.virtual_environment_node1_name
   virtual_environment_username = var.virtual_environment_username
   ssh_public_key = data.local_file.ssh_public_key.content
   }
-
-  module "komodo2" {
-  #source  = "app.terraform.io/d3-org/vm/pve"
-  source  = "./modules/komodo2"
-  virtual_environment_endpoint = var.virtual_environment_endpoint
-  virtual_environment_api_token = var.virtual_environment_api_token
-  virtual_environment_node2_name = var.virtual_environment_node2_name
-  virtual_environment_username = var.virtual_environment_username
-  ssh_public_key = data.local_file.ssh_public_key.content
-  } 
