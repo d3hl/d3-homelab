da05edba569a437757313c41c755da0794e18d81
Jay Dinh <d3tech@pm.me>
2025-09-08 06:31:11 +0800

    x

---
+++ modules/komodo1/komodo2.tf
@@ -0,0 +1,45 @@
+resource "proxmox_virtual_environment_file" "meta2_data_cloud_config" {
+  content_type = "snippets"
+  datastore_id = "cephfs"
+  node_name    = var.virtual_environment_node2_name
+
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    local-hostname: komodo2
+    EOF
+
+    file_name = "meta2-data-cloud-config.yaml"
+  }
+}
+
+
+resource "proxmox_virtual_environment_vm" "komodo2" {
+  name      = "komodo2"
+  node_name = var.virtual_environment_node1_name
+  tags      = sort(["debian", "terraform","komodo"])
+
+  clone {
+    vm_id = proxmox_virtual_environment_vm.debian-template.id
+  }
+
+  agent {
+    enabled = true
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    datastore_id = var.datastore_id
+    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
+    meta_data_file_id = proxmox_virtual_environment_file.meta2_data_cloud_config.id
+  }
+}
