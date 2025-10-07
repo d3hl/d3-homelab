5468f0112ee222d0e3c13544278ab3b200be5de6
d3HLlll <d3hl@pm.me>
2025-10-07 08:24:35 +0800

    x

--- terraform/modules/komodo/variables.tf
+++ terraform/modules/komodo/variables.tf
@@ -24,6 +24,11 @@ variable "virtual_environment_node_name" {
   type        = string
   default     = "pve10"
 }
+variable "virtual_environment_node1_name" {
+  description = "Name of the Proxmox node"
+  type        = string
+  default     = "pve10"
+}
 variable "virtual_environment_node2_name" {
   description = "Name of the Proxmox node"
   type        = string
