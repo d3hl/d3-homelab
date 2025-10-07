a99762020dfd5adfd1aa4682a452fcc6971e208d
d3HLlll <d3hl@pm.me>
2025-10-07 17:56:06 +0800

    x

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,7 +13,9 @@ terraform {
 }
     provider "proxmox" {
         endpoint = var.virtual_environment_endpoint
-        api_token = var.virtual_environment_api_token
+       # api_token = var.virtual_environment_api_token
+        username  = var.virtual_environment_username
+        password  = var.virtual_environment_password
         insecure = true 
     ssh {
       node {
