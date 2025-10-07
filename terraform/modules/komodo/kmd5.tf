333f7dc171390205da9f01e4f52542f4c5e4e5db
d3HLlll <d3hl@pm.me>
2025-10-07 18:41:36 +0800

    x

--- terraform/modules/komodo/kmd5.tf
+++ terraform/modules/komodo/kmd5.tf
@@ -1,4 +1,4 @@
-resource "proxmox_virtual_environment_file" "meta2_data_cloud_config" {
+resource "proxmox_virtual_environment_file" "meta5_data_cloud_config" {
   content_type = "snippets"
   datastore_id = "cephfs"
   node_name    = var.virtual_environment_node1_name
@@ -6,20 +6,20 @@ resource "proxmox_virtual_environment_file" "meta2_data_cloud_config" {
   source_raw {
     data = <<-EOF
     #cloud-config
-    local-hostname: kmd2
+    local-hostname: kmd5
     EOF
 
-    file_name = "meta2-data-cloud-config.yaml"
+    file_name = "meta5-data-cloud-config.yaml"
   }
 }
 
 
-resource "proxmox_virtual_environment_vm" "kmd2" {
-  name      = "kmd2"
-  node_name = var.virtual_environment_node2_name
+resource "proxmox_virtual_environment_vm" "kmd5" {
+  name      = "kmd5"
+  node_name = var.virtual_environment_node1_name
   pool_id = proxmox_virtual_environment_pool.komodo-pool.pool_id
   tags      = sort(["debian", "terraform", "komodo"])
-  migrate   = true
+  #migrate   = true
   clone {
     vm_id = proxmox_virtual_environment_vm.debian-template.id
   }
@@ -41,9 +41,9 @@ resource "proxmox_virtual_environment_vm" "kmd2" {
 
     datastore_id = var.datastore_id
     user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
-    meta_data_file_id = proxmox_virtual_environment_file.meta2_data_cloud_config.id
+    meta_data_file_id = proxmox_virtual_environment_file.meta5_data_cloud_config.id
   }
 }
-output "vm2_ipv4_address" {
-  value = proxmox_virtual_environment_vm.kmd2.ipv4_addresses[1][0]
+output "vm5_ipv4_address" {
+  value = proxmox_virtual_environment_vm.kmd5.ipv4_addresses[1][0]
 }
