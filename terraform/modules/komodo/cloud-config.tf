22b78326ad6046df696ea9383563fb46b6ddc13f
d3HLlll <d3hl@pm.me>
2025-10-09 03:23:27 +0800

    x

--- terraform/modules/komodo/cloud-config.tf
+++ terraform/modules/komodo/cloud-config.tf
@@ -1,10 +1,9 @@
 data "local_file" "ssh_public_key" {
   filename = "/home/d3/.ssh/d3_tf.pub" 
 }
-
 resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
   content_type = "snippets"
-  datastore_id = "cFS"
+  datastore_id = "cFSfs"
   node_name    = var.virtual_environment_nodeA_name
 
   source_raw {
