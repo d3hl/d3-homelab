e76bb0e2eff307edd2f90bf31889ecd4f8a3af72
d3HLlll <d3hl@pm.me>
2025-10-09 04:56:59 +0800

    x

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "komodo" {
   virtual_environment_endpoint = var.virtual_environment_endpoint
   virtual_environment_api_token = var.virtual_environment_api_token
   virtual_environment_username = var.virtual_environment_username
-  ssh_public_key = data.local_file.ssh_public_key.content
+  ssh_public_key = var.ssh_public_key
   }
 #module "lxc" {
 #  #source  = "app.terraform.io/d3-org/vm/pve"
