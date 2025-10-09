259cd864490ef9bf3194c60d9c9784d3cbdef84c
d3HLlll <d3hl@pm.me>
2025-10-09 18:49:42 +0800

    x

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,4 +13,5 @@ module "komodo" {
   virtual_environment_endpoint = var.virtual_environment_endpoint
   virtual_environment_api_token = var.virtual_environment_api_token
   virtual_environment_username = var.virtual_environment_username
+  ssh_public_key = var.ssh_public_key
 }
