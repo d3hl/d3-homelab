4e750d63ab109d6cdc9ee967b893ff15ba4eaf2b
d3HLlll <d3hl@pm.me>
2025-10-09 19:27:54 +0800

    x

--- terraform/modules/komodo/cloud-config.tf
+++ terraform/modules/komodo/cloud-config.tf
@@ -1,6 +1,6 @@
 data "local_file" "ssh_public_key" {
-  #filename = "/home/d3/.ssh/d3_tf.pub" 
-  filename = "${path.module}/d3_tf.pub"
+  filename = "/home/d3/.ssh/d3_tf.pub" 
+ # filename = "${path.module}/d3_tf.pub"
 }
 
 resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
