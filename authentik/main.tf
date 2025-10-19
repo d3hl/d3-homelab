7c4c1d17a86f57ccab97d15e34af1d5ae0c8fb12
d3HLlll <d3hl@pm.me>
2025-10-19 17:22:52 +0800

    x

--- authentik/main.tf
+++ authentik/main.tf
@@ -1,4 +1,14 @@
-module "applications" {
+module "proxmox" {
+  source = "./modules/applications"
+  AUTHENTIK_TOKEN = var.AUTHENTIK_TOKEN
+  AUTHENTIK_URL = var.AUTHENTIK_URL
+}
+module "immich" {
+  source = "./modules/applications"
+  AUTHENTIK_TOKEN = var.AUTHENTIK_TOKEN
+  AUTHENTIK_URL = var.AUTHENTIK_URL
+}
+module "beszel" {
   source = "./modules/applications"
   AUTHENTIK_TOKEN = var.AUTHENTIK_TOKEN
   AUTHENTIK_URL = var.AUTHENTIK_URL
