5fa53d3bb83b690a764d8d02a02e6165eb39c58c
d3HLlll <d3hl@pm.me>
2025-10-19 15:16:37 +0800

    x

--- authentik/provider.tf
+++ authentik/provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "authentik" {
   url   = "https://auth.d3adc3ii.cc"
-  token = var.AUTHENTIK_TOKEN               # Token should be defined in variables
+            # Token should be defined in variables
 }
--- authentik/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "AUTHENTIK_TOKEN" {
-    description = "The token used to authenticate with the Authentik API."
-    type        = string
-    sensitive   = true
-}
