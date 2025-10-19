d572550b71a0720df97f9752becccfe386d76b56
d3HLlll <d3hl@pm.me>
2025-10-19 16:51:01 +0800

    x

--- authentik/main.tf
+++ authentik/main.tf
@@ -1,4 +1,5 @@
 module "applications" {
   source = "./modules/applications"
-
+  AUTHENTIK_TOKEN = var.AUTHENTIK_TOKEN
+  AUTHENTIK_URL = var.AUTHENTIK_URL
 }
--- authentik/variables.tf
+++ authentik/variables.tf
@@ -1,5 +1,8 @@
 variable "AUTHENTIK_TOKEN" {
-    description = "The API token for the Authentik API"
+    type        = string
+    sensitive   = true
+    }
+variable "AUTHENTIK_URL" {
     type        = string
     sensitive   = true
     }
