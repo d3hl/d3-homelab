dd0c34df5b75344221ebbc8163cbe2eefd7ec7e0
d3HLlll <d3hl@pm.me>
2025-10-19 16:41:25 +0800

    x

--- authentik/main.tf
+++ authentik/main.tf
@@ -1,28 +1,4 @@
-# Create an application with a provider attached and policies applied
+module "applications" {
+  source = "./modules/applications"
 
-data "authentik_flow" "default-authorization-flow" {
-  slug = "default-provider-authorization-implicit-consent"
-}
-
-data "authentik_flow" "default-provider-invalidation-flow" {
-  slug = "default-provider-invalidation-flow"
-}
-resource "authentik_provider_oauth2" "provider_for_proxmox" {
-  name               = "Proxmox Provider"
-  client_id          = "proxmox"
-  client_secret      = "test"
-  allowed_redirect_uris = [
-    {
-      matching_mode = "strict",
-      url           = "https://10.10.10.10:8006/oauth2/callback",
-    }
-  ]
-  invalidation_flow = data.authentik_flow.default-provider-invalidation-flow.id
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-}
-
-resource "authentik_application" "proxmox" {
-  name              = "proxmox"
-  slug              = "proxmox"
-  protocol_provider = authentik_provider_oauth2.provider_for_proxmox.id
 }
---
+++ authentik/modules/applications/proxmox.tf
@@ -0,0 +1,27 @@
+# Create an application with a provider attached and policies applied
+
+data "authentik_flow" "default-authorization-flow" {
+  slug = "default-provider-authorization-implicit-consent"
+}
+
+data "authentik_flow" "default-provider-invalidation-flow" {
+  slug = "default-provider-invalidation-flow"
+}
+resource "authentik_provider_oauth2" "provider_for_proxmox" {
+  name               = "Proxmox Provider"
+  client_id          = "proxmox"
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict",
+      url           = "https://10.10.10.10:8006/oauth2/callback",
+    }
+  ]
+  invalidation_flow = data.authentik_flow.default-provider-invalidation-flow.id
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+}
+
+resource "authentik_application" "proxmox" {
+  name              = "proxmox"
+  slug              = "proxmox"
+  protocol_provider = authentik_provider_oauth2.provider_for_proxmox.id
+}
