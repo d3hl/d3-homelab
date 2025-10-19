3e0c8b60c112ee35bfe7ec4c0950b890bcdb8357
d3HLlll <d3hl@pm.me>
2025-10-19 16:13:56 +0800

    Refactor application setup to integrate OAuth2 provider and authorization flow

--- authentik/main.tf
+++ authentik/main.tf
@@ -1,20 +1,17 @@
-# Create an application entitlement bound to a group
-resource "authentik_application" "name" {
-  name = "example-app"
-  slug = "example-app"
+# Create an application with a provider attached and policies applied
+
+data "authentik_flow" "default-authorization-flow" {
+  slug = "default-provider-authorization-implicit-consent"
+}
+resource "authentik_provider_oauth2" "provider_for_proxmox" {
+  name               = "proxmox"
+  client_id          = "example-app"
+  client_secret      = "test"
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
 }
 
-resource "authentik_application_entitlement" "ent" {
-  name        = "test-ent"
-  application = authentik_application.name.id
-}
-
-resource "authentik_group" "group" {
-  name = "test-ent-group"
-}
-
-resource "authentik_policy_binding" "test-ent-access" {
-  target = authentik_application_entitlement.ent.id
-  group  = authentik_group.group.id
-  order  = 0
+resource "authentik_application" "proxmox" {
+  name              = "proxmox"
+  slug              = "proxmox"
+  protocol_provider = authentik_provider_oauth2.provider_for_proxmox.id
 }
