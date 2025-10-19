5d37f694a995c03b2afd22a8cf32c0442da58ab8
d3HLlll <d3hl@pm.me>
2025-10-20 04:25:05 +0800

    Add Beszel application with OAuth2 provider and policy binding for d3-admins group

--- authentik/modules/applications/beszel/main.tf
+++ authentik/modules/applications/beszel/main.tf
@@ -1,4 +1,7 @@
 # Create an application with a provider attached and policies applied
+data "authentik_group" "d3-admins" {
+  name = "d3-admins"
+}
 
 data "authentik_flow" "default-authorization-flow" {
   slug = "default-provider-authorization-implicit-consent"
@@ -13,7 +16,7 @@ resource "authentik_provider_oauth2" "provider_for_beszel" {
   allowed_redirect_uris = [
     {
       matching_mode = "strict",
-      url           = "https://10.10.10.10:8006/oauth2/callback",
+      url           = "https://beszel.d3adc3ii.cc/api/oauth2-redirect"
     }
   ]
   invalidation_flow = data.authentik_flow.default-provider-invalidation-flow.id
@@ -24,4 +27,12 @@ resource "authentik_application" "beszel" {
   name              = "beszel"
   slug              = "beszel"
   protocol_provider = authentik_provider_oauth2.provider_for_beszel.id
+  group            = data.authentik_group.d3-admins.id
+}
+
+# Binding policy to application to allow access to specific group
+resource "authentik_policy_binding" "app-access" {
+  target = authentik_application.beszel.uuid
+  group  = data.authentik_group.d3-admins.id
+  order  = 0
 }
