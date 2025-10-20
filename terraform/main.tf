25a912f6b2b13ccd03b3677016489b79b30e23ed
d3HLlll <d3hl@pm.me>
2025-10-20 20:31:22 +0800

    Refactor Supermicro module configuration in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,12 @@ module "komodo" {
   virtual_environment_username = var.virtual_environment_username
 
 }
+
+module "Supermicro" {
+  source = "./modules/Supermicro"
+  virtual_environment_endpoint = var.virtual_environment_endpoint
+  virtual_environment_api_token = var.virtual_environment_api_token
+  virtual_environment_username = var.virtual_environment_username
+
+}
+
