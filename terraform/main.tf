51ba6e7d51cff4b5eea2a0306ba64b4685721639
d3HLlll <d3hl@pm.me>
2025-10-21 02:08:12 +0800

    x

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,19 @@
+# Add module blocks for each of your modules here
 module "komodo" {
-  source = "./modules/resources/komodo"
-  virtual_environment_endpoint = var.virtual_environment_endpoint
+  source                        = "./modules/resources/komodo"
+  virtual_environment_endpoint  = var.virtual_environment_endpoint
   virtual_environment_api_token = var.virtual_environment_api_token
-  virtual_environment_username = var.virtual_environment_username
+  virtual_environment_username  = var.virtual_environment_username
 
 }
 
 #module "Supermicro" {
- # source = "./modules/Supermicro"
-  #virtual_environment_endpoint = var.virtual_environment_endpoint
-  #virtual_environment_api_token = var.virtual_environment_api_token
-  #virtual_environment_username = var.virtual_environment_username
-  # pass the debian cloud image file id exported by the komodo module
-  #debian_cloud_image_file_id = module.komodo.debian_cloud_image_file_id
+# source = "./modules/Supermicro"
+#virtual_environment_endpoint = var.virtual_environment_endpoint
+#virtual_environment_api_token = var.virtual_environment_api_token
+#virtual_environment_username = var.virtual_environment_username
+# pass the debian cloud image file id exported by the komodo module
+#debian_cloud_image_file_id = module.komodo.debian_cloud_image_file_id
 
 #}
 
