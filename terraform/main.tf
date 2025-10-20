66f86806fa60ea5a813616663fef25f0454eebc8
d3HLlll <d3hl@pm.me>
2025-10-21 03:43:18 +0800

    Refactor Proxmox provider configuration and move to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,36 @@
+
+provider "proxmox" {
+  endpoint  = var.virtual_environment_endpoint
+  api_token = var.virtual_environment_api_token
+  #username  = var.virtual_environment_username
+  #password  = var.virtual_environment_password
+  insecure = true
+  ssh {
+    node {
+      name    = "pve10"
+      address = "10.10.10.10"
+    }
+    node {
+      name    = "pve14"
+      address = "10.10.10.14"
+    }
+    node {
+      name    = "nodeA"
+      address = "10.10.10.18"
+    }
+    node {
+      name    = "nodeB"
+      address = "10.10.10.15"
+    }
+    node {
+      name    = "nodeC"
+      address = "10.10.10.19"
+    }
+    agent    = true
+    username = var.virtual_environment_username
+  }
+}
+#
 module "komodo" {
   source                        = "./resources/komodo"
   virtual_environment_username  = var.virtual_environment_username
