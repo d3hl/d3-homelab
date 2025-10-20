00407c6e80283af2ba5450eabf6120ab3f33d38b
d3HLlll <d3hl@pm.me>
2025-10-21 03:52:40 +0800

    sPlease enter the commit message for your changes. Lines starting

--- terraform/modules/meta/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "meta_data_cloud_config" {
-  description = "The proxmox_virtual_environment_file resource for meta-data cloud-config"
-  value       = proxmox_virtual_environment_file.meta_data_cloud_config
-}
-
-output "meta_data_file_id" {
-  description = "The ID of the meta-data cloud-config file"
-  value       = proxmox_virtual_environment_file.meta_data_cloud_config.id
-}
