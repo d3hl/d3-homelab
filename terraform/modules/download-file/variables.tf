3ac65a56801e73a1affed100b384f5ed6c084248
d3HLlll <d3hl@pm.me>
2025-10-22 03:51:35 +0800

    x

--- terraform/modules/download-file/variables.tf
+++ terraform/modules/download-file/variables.tf
@@ -3,3 +3,26 @@ variable "virtual_environment_node_name" {
   type        = string
   default     = "nodeA"
 }
+variable "datastore_id" {
+  type        = string
+  description = "Datastore for VM disks"
+  default     = "cephVM"
+}
+variable "virtual_environment_endpoint" {
+  type = string
+}
+
+variable "virtual_environment_username" {
+  description = "Proxmox User for API Access"
+  type        = string
+}
+
+
+variable "virtual_environment_api_token" {
+  description = "The API token for the Proxmox Virtual Environment API"
+  type        = string
+  sensitive   = true
+}
+
+
+
