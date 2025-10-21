06742dd97351d227cb3e193f07d986494734af75
d3HLlll <d3hl@pm.me>
2025-10-21 13:24:40 +0800

    Fix output syntax for vm_ipv4_address in output.tf

--- terraform/resources/kmd1/output.tf
+++ terraform/resources/kmd1/output.tf
@@ -1,3 +1,3 @@
 output "vm_ipv4_address" {
-  value = proxmox_virtual_environment_vm.${var.hostname}.ipv4_addresses[1][0]
+  value = proxmox_virtual_environment_vm[var.hostname].ipv4_addresses[1][0]
 }
