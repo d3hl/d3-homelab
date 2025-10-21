5fcec85d0c3b9f7393f199201136f694cf00d2d8
d3HLlll <d3hl@pm.me>
2025-10-21 22:37:28 +0800

    x

--- terraform/modules/template/main.tf
+++ terraform/modules/template/main.tf
@@ -61,7 +61,7 @@ resource "proxmox_virtual_environment_vm" "debian_template" {
       }
     }
 
-    user_data_file_id = module.cloud-init.user_data_cloud_init_id
+    user_data_file_id = module.cloud-init.user_data_cloud_init_file_id
   }
   disk {
     datastore_id = "cephVM"
