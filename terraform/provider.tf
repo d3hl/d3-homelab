f8d50d9fbc31c7f05efdacc57a4d9fe417a37d7d
d3HLlll <d3hl@pm.me>
2025-10-21 11:42:37 +0800

    x

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.5.3"
-    }
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.85.0" # x-release-please-version
