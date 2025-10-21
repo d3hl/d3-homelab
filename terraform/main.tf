6d3c1365dc6d8c6b63e8e3a67f90a03631a82dcf
d3HLlll <d3hl@pm.me>
2025-10-22 03:37:27 +0800

    Remove Proxmox provider configuration and add .terraform.lock.hcl file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,4 @@
-provider "proxmox" {
-  endpoint  = var.virtual_environment_endpoint
-  api_token = var.virtual_environment_api_token
 
-  insecure = true
-  ssh {
-    node {
-      name    = "pve10"
-      address = "10.10.10.10"
-    }
-    node {
-      name    = "pve14"
-      address = "10.10.10.14"
-    }
-    node {
-      name    = "nodeA"
-      address = "10.10.10.18"
-    }
-    node {
-      name    = "nodeB"
-      address = "10.10.10.15"
-    }
-    node {
-      name    = "nodeC"
-      address = "10.10.10.19"
-    }
-    agent    = true
-    username = var.virtual_environment_username
-  }
-}
 #
 #module "kmd1" {
 #  source                        = "./resources/kmd1"
