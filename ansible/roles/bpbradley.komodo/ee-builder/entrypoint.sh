7cfd2ad0aec01e9cffc561a0f790e10a26ec9a71
d3HLlll <d3hl@pm.me>
2025-12-27 14:04:54 +0800

    Remove obsolete configuration files and sensitive information from the repository, including worker.yaml, proxmox_config.yaml, kubeconfig files, and talosconfig.yaml.

--- ansible/roles/bpbradley.komodo/ee-builder/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-[[ -n "$VAULT_PASS" && -z "$ANSIBLE_VAULT_PASSWORD_FILE" ]] \
-    && export ANSIBLE_VAULT_PASSWORD_FILE=/etc/ansible/.vault_pass.sh
-
-exec dumb-init -- /usr/bin/entrypoint "$@"
