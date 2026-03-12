86af99ae265a3f54f1ca2e7e13ae988624f329f3
d3HLlll <d3hl@pm.me>
2026-03-13 07:48:57 +0800

    x

--- terraform/modules/debian/cloud-config.tf
+++ terraform/modules/debian/cloud-config.tf
@@ -1,5 +1,5 @@
 data "local_file" "ssh_public_key" {
-  filename = "/home/d3/.ssh/d3_tf.pub"
+  filename = "/home/d3/.ssh/kmd-2.pub"
 }
 
 resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
