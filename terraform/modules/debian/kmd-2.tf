fc2ac79c3e06c464ee37f73886d92e3dcfd30a57
d3HLlll <d3hl@pm.me>
2026-03-13 06:54:28 +0800

    xxx

--- terraform/modules/debian/kmd-2.tf
+++ terraform/modules/debian/kmd-2.tf
@@ -16,29 +16,29 @@ resource "proxmox_virtual_environment_cloned_vm" "debian_clone" {
       iothread     = true
       ssd          = true
     }
-
-    # Only explicitly listed devices are managed
-    # Network device inherited from template is preserved but not managed
-    # To manage it, explicitly list it here:
-    # Only manage the first network interface
-    network = {
-      net0 = {
-        bridge = "vmbr0"
-        model  = "virtio"
-        tag    = 10 # Add VLAN tag to net0
-      }
-    }
-
-    # Memory configuration using new terminology
-    memory = {
-      size    = 16384 # Total memory available to VM
-      balloon = 512   # Minimum guaranteed memory via balloon device
-    }
-
-    cpu = {
-      cores = 4
+  }
+  # Only explicitly listed devices are managed
+  # Network device inherited from template is preserved but not managed
+  # To manage it, explicitly list it here:
+  # Only manage the first network interface
+  network = {
+    net0 = {
+      bridge = "vmbr0"
+      model  = "virtio"
+      tag    = 10 # Add VLAN tag to net0
     }
   }
+
+  # Memory configuration using new terminology
+  memory = {
+    size    = 16384 # Total memory available to VM
+    balloon = 512   # Minimum guaranteed memory via balloon device
+  }
+
+  cpu = {
+    cores = 4
+  }
+
 }
 
 output "vm_id" {
