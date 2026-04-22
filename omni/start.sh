17db8efda6bf1b308f22a43e53c28be500ceb4b6
d3HLlll <d3hl@pm.me>
2026-04-23 03:34:56 +0800

    fix: Correct file reference for Cloudflare template in start script

--- omni/start.sh
+++ omni/start.sh
@@ -1,3 +1,3 @@
 TARGET_ENV="d3hl" op inject -f -i ".env.tpl" -o ".env"
-TARGET_ENV="d3hl" op inject -f -i ".cloudflare.tpl" -o "cloudflare.ini"
+TARGET_ENV="d3hl" op inject -f -i "cloudflare.tpl" -o "cloudflare.ini"
 TARGET_ENV="d3hl" op inject -f -i "omni.tpl" -o "omni.asc"
