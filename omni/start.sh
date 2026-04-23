582727d73c4ca5e9d3a6252fcaacb6af7e9b7ac9
d3HLlll <d3hl@pm.me>
2026-04-24 00:31:55 +0800

    fix: Correct shebang line in start.sh

--- omni/start.sh
+++ omni/start.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 TARGET_ENV="d3hl" op inject -f -i ".env.tpl" -o ".env"
 #TARGET_ENV="d3hl" op inject -f -i "cloudflare.tpl" -o "cloudflare.ini"
 TARGET_ENV="d3hl" op inject -f -i "./omni.tpl" -o "omni.asc"
