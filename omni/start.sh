392fbd034254592869740f1ee92649608f7d85cb
d3HLlll <d3hl@pm.me>
2026-04-23 23:33:51 +0800

    fix: Correct path for omni.tpl in start script

--- omni/start.sh
+++ omni/start.sh
@@ -1,7 +1,7 @@
 !/bin/bash
 TARGET_ENV="d3hl" op inject -f -i ".env.tpl" -o ".env"
 #TARGET_ENV="d3hl" op inject -f -i "cloudflare.tpl" -o "cloudflare.ini"
-TARGET_ENV="d3hl" op inject -f -i "./certs/omni.tpl" -o "./certs/omni.asc"
+TARGET_ENV="d3hl" op inject -f -i "./omni.tpl" -o "omni.asc"
 TARGET_ENV="d3hl" op inject -f -i "./certs/server-chain.pem.tpl" -o "server-chain.pem"
 TARGET_ENV="d3hl" op inject -f -i "./certs/server-key.pem.tpl" -o "server-key.pem"
 TARGET_ENV="d3hl" op inject -f -i "./certs/ca.pem.tpl" -o "ca.pem"
