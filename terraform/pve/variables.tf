7eb5c1dacfe4c8f600ea3298f1410dbb22402ca3
d3HLlll <d3hl@pm.me>
2026-04-26 01:49:47 +0800

    Revert "feat: Enhance Proxmox configuration with new VM and variable definitions for omni-master deployment"
    This reverts commit c06478dff14c40681ff7cbff4c37bdd4f733acde.

--- terraform/pve/variables.tf
+++ terraform/pve/variables.tf
@@ -1,47 +1,33 @@
 variable "virtual_environment_endpoint" {
-  description = "Proxmox API endpoint URL (e.g. https://10.10.10.10:8006/)"
-  type        = string
+  type = string
 }
 
 variable "virtual_environment_api_token" {
-  description = "Proxmox API token (format: user@realm!tokenid=secret)"
+  description = "The API token for the Proxmox Virtual Environment API"
   type        = string
   sensitive   = true
 }
 
-variable "virtual_environment_username" {
-  description = "Username for Proxmox SSH operations"
-  type        = string
-  default     = "d3"
-}
-
-variable "ssh_public_key_file" {
-  description = "Path to the SSH public key injected into VMs via cloud-init"
-  type        = string
-  default     = "/home/d3/.ssh/d3_tf.pub"
-}
 
 variable "virtual_environment_node_name" {
   description = "Default Proxmox node for this workspace (nodeD)"
   type        = string
   default     = "nodeD"
 }
-
-variable "virtual_environment_node_nodeA" {
-  description = "Proxmox node A"
+variable "virtual_environment_nodeA" {
+  description = "Name of the Proxmox node"
   type        = string
   default     = "nodeA"
 }
-
 variable "virtual_environment_node_nodeF" {
-  description = "Proxmox node F"
+  description = "Name of the Proxmox node"
   type        = string
   default     = "nodeF"
 }
-
 variable "datastore_id" {
   description = "Datastore for VM disks (Ceph RBD)"
   type        = string
+  description = "Datastore for VM disks"
   default     = "cephVM"
 }
 
@@ -52,7 +38,6 @@ variable "snippets_datastore_id" {
 }
 
 variable "ubuntu_template" {
-  description = "Ubuntu template VM ID used as clone source"
-  type        = number
-  default     = 999
+  type    = string
+  default = "999"
 }
