1858b53e775158b87595502e431f115a105b977c
d3HLlll <d3hl@pm.me>
2026-05-07 22:42:20 +0800

    fix: Add environment variable exports to start.sh for configuration

--- omni/start.sh
+++ omni/start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+export HOST_PUBLIC_IP=10.10.10.40
+export HOST_PRIVATE_IP=10.11.11.40
+export OMNI_ENDPOINT=omni.d3hl.site
+export AUTH_ENDPOINT=auth.d3hl.site
+export OMNI_USER_EMAIL="d3tech@pm.me"
+
+
 TARGET_ENV="d3hl" op inject -f -i ".env.tpl" -o ".env"
 #TARGET_ENV="d3hl" op inject -f -i "cloudflare.tpl" -o "cloudflare.ini"
 TARGET_ENV="d3hl" op inject -f -i "./omni.tpl" -o "omni.asc"
