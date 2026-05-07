b2fce04681b3039831e7541ff1fe07c8ea88ab9b
d3HLlll <d3hl@pm.me>
2026-05-07 23:10:47 +0800

    fix: Add setup script for environment configuration and initialization

---
+++ omni/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export HOST_PUBLIC_IP=10.10.10.40
+export HOST_PRIVATE_IP=10.11.11.40
+export OMNI_ENDPOINT=omni.d3hl.site
+export AUTH_ENDPOINT=auth.d3hl.site
+export OMNI_USER_EMAIL="d3tech@pm.me"
+
+bash ./scripts/cfssl.sh
+bash ./ca.sh
+bash ./setup-gbg.sh 
